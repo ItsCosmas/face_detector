14c2ccd1fe205f87983ee8f1fef89b88dc65ada3
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-12 14:04:11 +0300

    Update Image Path

--- face_detector.py
+++ face_detector.py
@@ -1,6 +1,7 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+# img path
 img = cv2.imread(r"cozy.jpg")
 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
